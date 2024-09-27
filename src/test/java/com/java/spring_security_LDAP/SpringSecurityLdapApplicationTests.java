0ac30f85d624fa6d833caf61e4dbf846c1116b3a
subramanyam123-c <subramanyamb30@gmail.com>
2024-09-26 20:45:40 -0500

    Customised security allowing users that are in LDAP file(ldif) by using LDAP authentication

---
+++ src/test/java/com/java/spring_security_LDAP/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.spring_security_LDAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
